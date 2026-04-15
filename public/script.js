9009924fac736e99e7b2d012f001203ac7e2d1bd
Mohitsingal <mohitsingal2005@gmail.com>
2026-04-15 19:36:54 +0530

    TASK 5

--- public/script.js
+++ public/script.js
@@ -1,116 +1,191 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("myform");
+
+    // SUBMIT
+    form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        let name = document.getElementById("username").value.trim();
+        let email = document.getElementById("useremail").value.trim();
+        let phone = document.getElementById("phonenumber").value.trim();
+        let password = document.getElementById("password").value;
+        let confirmPassword = document.getElementById("confirmPassword").value;
+        let dob = document.getElementById("dob").value;
+        let genderElement = document.querySelector('input[name="gender"]:checked');
+        let address = document.getElementById("address").value.trim();
+
+        let valid = true;
+
+        
+        document.querySelectorAll("small").forEach(e => e.innerText = "");
+
+        // VALIDATION 
+
+        if (name === "") {
+            document.getElementById("nameError").innerText = "Name is required";
+            valid = false;
+        }
+
+        if (!email.includes("@")) {
+            document.getElementById("emailError").innerText = "Invalid email";
+            valid = false;
+        }
+
+        if (phone.length !== 10 || isNaN(phone)) {
+            document.getElementById("phoneError").innerText = "Phone must be 10 digits";
+            valid = false;
+        }
+
+        if (
+            password.length < 6 ||
+            !/[A-Z]/.test(password) ||
+            !/[0-9]/.test(password) ||
+            !/[!@#$%^&*]/.test(password)
+        ) {
+            document.getElementById("passwordError").innerText =
+                "Password must include uppercase, number & symbol";
+            valid = false;
+        }
+
+        if (password !== confirmPassword) {
+            document.getElementById("confirmError").innerText = "Passwords do not match";
+            valid = false;
+        }
+
+        if (!dob) {
+            document.getElementById("dobError").innerText = "Select DOB";
+            valid = false;
+        }
+
+        if (!genderElement) {
+            document.getElementById("genderError").innerText = "Select gender";
+            valid = false;
+        }
+
+        if (address === "") {
+            document.getElementById("addressError").innerText = "Address required";
+            valid = false;
+        }
+
+        // SenD to BACKEND
+        if (valid) {
+
+            const userData = {
+                name,
+                email,
+                phone,
+                password,
+                dob,
+                gender: genderElement.value,
+                address
+            };
 
 
-const form = document.getElementById("myform");
+            try {
+                const response = await fetch('/users', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(userData)
+                });
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
+                const data = await response.json();
 
-    let name= document.getElementById("username").value.trim();
-    let email = document.getElementById("useremail").value.trim();
-    let phone = document.getElementById("phonenumber").value.trim();
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
-    let dob = document.getElementById("dob").value;
-    let gender = document.querySelector('input[name="gender"]:checked').value;
-    let address = document.getElementById("address").value;
+                document.getElementById("result").innerText =
+                    "User registered successfully 🎉";
+
+                console.log(data);
+                   loadUsers();
+
+
+            } catch (err) {
+                console.log("Error:", err);
+            }
+
+
+        }
+
+
+    });
+
+    // LIVE VALIDATION 
+
+    document.getElementById("username").addEventListener("input", function () {
+        document.getElementById("nameError").innerText =
+            this.value.trim() === "" ? "Name is required" : "";
+    });
+
+    document.getElementById("useremail").addEventListener("input", function () {
+        document.getElementById("emailError").innerText =
+            this.value.includes("@") ? "" : "Invalid email";
+    });
+
+    document.getElementById("phonenumber").addEventListener("input", function () {
+        document.getElementById("phoneError").innerText =
+            this.value.length === 10 && !isNaN(this.value) ? "" : "Enter 10 digits";
+    });
+
+    document.getElementById("password").addEventListener("input", function () {
+        let password = this.value;
+        let error = "";
+
+        if (password.length < 6) error = "Min 6 characters";
+        else if (!/[A-Z]/.test(password)) error = "Need uppercase";
+        else if (!/[0-9]/.test(password)) error = "Need number";
+        else if (!/[!@#$%^&*]/.test(password)) error = "Need special char";
+
+        document.getElementById("passwordError").innerText = error;
+    });
+
+    document.getElementById("confirmPassword").addEventListener("input", function () {
+        let password = document.getElementById("password").value;
+
+        document.getElementById("confirmError").innerText =
+            this.value === password ? "" : "Passwords do not match";
+    });
+
+    loadUsers();
+
+
     
-    let valid = true;
-
-    document.querySelectorAll("small").forEach(e => e.innerText = "");
-
-    if(name === "") {
-        document.getElementById("nameError").innerText = "Name is required";
-        valid = false; 
-    }
-
-    if(!email.includes("@")) {
-        document.getElementById("emailError").innerText = "Invalid email";
-        valid = false;
-    }
-
-    if(phone.length !== 10 || isNaN(phone)) {
-        document.getElementById("phoneError").innerText = "Phone number must be 10 digits";
-        valid = false;
-    }
-
-    if(password.length < 6 ) {
-        document.getElementById("passwordError").innerText = "Password must be atleast 6 characters"
-        valid = false;
-    }
-
-    if(password !== confirmPassword) {
-        document.getElementById("confirmError").innerText = "Passwords do not match";
-        valid = false;
-    }
-
-    if(!dob){
-        document.getElementById("dobError").innerText = "Select DOB";
-        valid = false;
-    }
-
-    if(!gender) {
-        document.getElementById("genderError").innerText = "Select gender";
-        valid = false;
-    }
-
-    if(address === "") {
-        document.getElementById("addressError").innerText = "Address required";
-        valid = false;
-    }
-
-    if(valid) {
-        document.getElementById("result").innerText = `Welcome ${name}! Registration is sucessful`;
-    }
-
-    // Live validation
-
-document.getElementById("username").addEventListener("input", function () {
-    let name = this.value.trim();
-    document.getElementById("nameError").innerText =
-        name === "" ? "Name is required" : "";
 });
 
-document.getElementById("useremail").addEventListener("input", function () {
-    let email = this.value;
-    document.getElementById("emailError").innerText =
-        email.includes("@") ? "" : "Invalid email";
-});
+async function loadUsers() {
+    try {
+        const response = await fetch('/users');
+        const users = await response.json();
 
-document.getElementById("phonenumber").addEventListener("input", function () {
-    let phone = this.value;
-    document.getElementById("phoneError").innerText =
-        phone.length === 10 && !isNaN(phone) ? "" : "Enter 10 digit number";
-});
+        const userList = document.getElementById("userList");
+        userList.innerHTML = "";
 
-document.getElementById("password").addEventListener("input", function () {
-    let password = this.value;
-    let error = "";
+        users.forEach(user => {
+            const div = document.createElement("div");
 
-    if (password.length < 6) {
-        error = "Minimum 6 characters required";
-    } else if (!/[A-Z]/.test(password)) {
-        error = "Must include at least 1 uppercase letter";
-    } else if (!/[0-9]/.test(password)) {
-        error = "Must include at least 1 number";
-    } else if (!/[!@#$%^&*]/.test(password)) {
-        error = "Must include at least 1 special character";
+            div.className = "card p-3 mb-2";
+
+            div.innerHTML = `
+                <strong>${user.name}</strong> - ${user.email} - ${user.phone}
+                <button onclick="deleteUser(${user.id})" class="btn btn-danger btn-sm float-end">
+                    Delete
+                </button>
+            `;
+
+            userList.appendChild(div);
+        });
+
+    } catch (err) {
+        console.log(err);
     }
+}
+async function deleteUser(id) {
+    try {
+        await fetch(`/users/${id}`, {
+            method: 'DELETE'
+        });
 
-    document.getElementById("passwordError").innerText = error;
-});
+        loadUsers(); // refresh list
 
-document.getElementById("confirmPassword").addEventListener("input", function () {
-    let password = document.getElementById("password").value;
-    let confirm = this.value;
-
-    document.getElementById("confirmError").innerText =
-        password === confirm ? "" : "Passwords do not match";
-});
-
-console.log("Form Submitted");
-
-});
-
-});
-
+    } catch (err) {
+        console.log(err);
+    }
+}
